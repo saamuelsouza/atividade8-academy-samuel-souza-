9150b8dd1a9b10f61258b45c9e92c150f37bdad4
Samuel Souza <saamuelfelipe@gmail.com>
2024-05-23 17:09:26 -0300

    Configurações iniciais feature Cadastro.

---
+++ cypress/support/steps/cadastro.step.js
@@ -0,0 +1,4 @@
+import { Given, When, Then, Before, After } from '@badeball/cypress-cucumber-preprocessor';
+import { faker } from '@faker-js/faker';
+import CadastroPage from '../page/cadastro.page';
+const paginaCadastro = new CadastroPage();
