7d3de15be68e19c0863260acbd0fc227ce85a898
Samuel Souza <saamuelfelipe@gmail.com>
2024-05-23 16:50:15 -0300

    Configurações cucumber preprocessor em cyoress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
+const { addCucumberPreprocessorPlugin } = require ('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+      apiBaseUrl: ''
     },
+    specPattern: 'cypress/e2e/**/*.feature',
+    async setupNodeEvents(on, config){
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on('file:preprocessor', createBundler({ plugins: [createEsbuildPlugin(config)]}));
+
+      return config;
+
+    }
   },
 });
